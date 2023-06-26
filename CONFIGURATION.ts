0820469c7096b8adf59d408971c56918b380ab7c
Mushon Zer-Aviv <mushon@shual.com>
2023-06-26 10:01:09 +0300

    Update CONFIGURATION.ts

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -1,6 +1,6 @@
 export const AIRTABLE_BASE = 'app1XEyXqNSWQnhTM';
 export const AIRTABLE_API_KEY = 'keyyCB522llXkLKKE';
-export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shryuZ4FQQYP5VUAP';
+export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrBLMz2DgkOrCu7Q';
 
 export const MAPBOX_STYLE = 'mapbox://styles/atlasmedliq/cl59k9bun000y14rzt3yy5ljt';
 export const MAPBOX_BASE_STYLE = 'mapbox://styles/atlasmedliq/ckiodibg82x1k17tcof8e9pmd';
