9209b8f5e53c8ef5edc0e9a140ce872cc0e8994f
Mushon Zer-Aviv <mushon@shual.com>
2023-06-26 10:36:58 +0300

    mapbox update

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -2,9 +2,9 @@ export const AIRTABLE_BASE = 'apphNsrk4Re3sfGxo';
 export const AIRTABLE_API_KEY = 'keyyCB522llXkLKKE';
 export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrBLMz2DgkOrCu7Q';
 
-export const MAPBOX_STYLE = 'mapbox://styles/atlasmedliq/cl59k9bun000y14rzt3yy5ljt';
-export const MAPBOX_BASE_STYLE = 'mapbox://styles/atlasmedliq/ckiodibg82x1k17tcof8e9pmd';
-export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXRsYXNtZWRsaXEiLCJhIjoiY2tpbXgzNW5qMHhhcjJ5cGtydHpkNnJqYyJ9.vfietFuvTA8S1vaGlm3CUQ';
+export const MAPBOX_STYLE = 'mapbox://styles/otcrew/cljb7mpjw003c01o44gwegzv9' // 'mapbox://styles/atlasmedliq/cl59k9bun000y14rzt3yy5ljt';
+export const MAPBOX_BASE_STYLE = 'mapbox://styles/otcrew/cljb7mpjw003c01o44gwegzv9' // 'mapbox://styles/atlasmedliq/ckiodibg82x1k17tcof8e9pmd';
+export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoib3RjcmV3IiwiYSI6ImNsaXQ3aTF4cTA1dmkzaG15cG50N3VsZXcifQ.dW-QCJWKjhmd1PAvJsZrMw'; // 'pk.eyJ1IjoiYXRsYXNtZWRsaXEiLCJhIjoiY2tpbXgzNW5qMHhhcjJ5cGtydHpkNnJqYyJ9.vfietFuvTA8S1vaGlm3CUQ';
 
 export const HORIZONTAL_LAYOUT = true; // enable for horizontal layout
 
